6b255b980e45257378b742b9f168671bdc893b73
Campbell20 <jcampbell145@alamo.edu>
2018-02-23 22:07:48 -0600

    Added map

--- BikeRackChecker/Scripts/MyMap.js
+++ BikeRackChecker/Scripts/MyMap.js
@@ -6,10 +6,5 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2FtcGJlbGwyMCIsImEiOiJjamUwbjFxNm41NTQ1MnFxcDhicnFha3ZjIn0.KyYq-lCDKDjOvL0VHZs0Mg';
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10',
-    // initial position in [lon, lat] format
-    //29.4241° N, 98.4936° W -- San Antonio
-    center: [29.4241, 98.4936],
-    // initial zoom
-    zoom: 14
+    style: 'mapbox://styles/campbell20/cje0qofkk333a2rqfzfto97o8'
 });
